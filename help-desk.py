bb2f19a04334c66e211e97786abbf2dce468e3fd
GabrielRochadeLima <gabrielrochadelima@hotmail.com>
2026-08-18 18:00:20 -0300

    Create hel-desk system

---
+++ help-desk.py
@@ -0,0 +1,27 @@
+numeros_chamados = 1
+
+def abrir_chamado():
+    global numeros_chamados
+    print("Chamado aberto com sucesso!")
+    print("Número do chamado: ", numeros_chamados)
+    numeros_chamados += 1
+
+print("Bem vindo ao Help desk")
+print("Escolha uma das opções abaixo:")
+print("1 - Abrir chamado")
+print("2 - Consultar chamados")
+print("3 - Sair")
+
+numero_digitado = int(input("Digite o número da opção desejada: "))
+if numero_digitado == 1:
+    print("Abrindo chamado...")
+    abrir_chamado()
+elif numero_digitado == 2:
+    print("Consultando chamados...")
+elif numero_digitado == 3:
+    print("Saindo...")
+else:
+    print("Opção inválida. Por favor, escolha uma opção válida.")
+
+
+
