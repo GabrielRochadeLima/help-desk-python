9221b7b39aa95d9fdd83b1ccc8782d731bb10001
GabrielRochadeLima <gabrielrochadelima@hotmail.com>
2026-08-20 14:32:55 -0300

    fix: ajuste de funções e main

--- help-desk.py
+++ help-desk.py
@@ -19,19 +19,36 @@ def menu_inicial():
     print("=====Bem vindo ao Help desk=====")
 
     print("Escolha uma das opções abaixo:")
-    print("1 - Abrir chamado")
-    print("2 - Consultar chamados")
-    print("3 - Sair")
+    print("1 - login")
+    print("2 - Cadastrar usuário")
+    print("3 - Abrir chamado")
+    print("4 - Consultar chamados")
+    print("5 - Sair")
 
     numero_digitado = int(input("Digite o número da opção desejada: "))
     if numero_digitado == 1:
         abrir_chamado()
+
     elif numero_digitado == 2:
         print("Consultando chamados...")
     elif numero_digitado == 3:
         print("Saindo...")
     else:
         print("Opção inválida. Por favor, escolha uma opção válida.")
+
+def menu_estatico():
+    print("=====Bem vindo ao Help desk=====")
+
+    print("Escolha uma das opções abaixo:")
+    print("1 - Abrir chamado")
+    print("2 - Consultar chamados")
+    print("3 - Sair")
+
+def login():
+    nome_usuario = input("Digite seu nome de usuário: ")
+    email = input("Digite seu email: ")
+    senha = input("Digite sua senha: ")
+    print(f"Login realizado com sucesso para o usuário {nome_usuario}!")
 
 def cadastrar_usuario():
     nome = input("Digite seu nome: ")
@@ -41,15 +58,14 @@ def cadastrar_usuario():
 
 def main():
     while True:
-        print("Olá usuário! Bem vindo ao help desk!")
-        print("É a sua primeira vez aqui?")
-        op= int(input("Digite 1 para sim e 2 para não: "))
+        menu_estatico()
+        op = int(input("Digite o número da opção desejada: "))
         if op == 1:
             print("Seja bem vindo!")
-            cadastrar_usuario()
+            login()
         elif op == 2:
             print("Seja bem vindo de volta!")
-            menu_inicial()
+            menu_estatico()
         else:
             print("Opção inválida. Por favor, escolha uma opção válida.")    
 
