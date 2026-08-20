a4286aa23964e39f41196051cf7447ce30fb1b6f
GabrielRochadeLima <gabrielrochadelima@hotmail.com>
2026-08-20 15:58:05 -0300

    fix: Ajuste de função de login e arquivo user

--- help-desk.py
+++ help-desk.py
@@ -1,76 +1,118 @@
-numeros_chamados = 1
-
 def abrir_chamado():
     try:
         with open("chamados.txt", "r") as arquivo:
             linhas = arquivo.readlines()
-        numeros_chamados = len(linhas) + 1
+
+        numero_chamado = len(linhas) + 1
+
     except FileNotFoundError:
-        numeros_chamados = 1    
-    print("Chamado aberto com sucesso!")
-    print("Número do chamado: ", numeros_chamados)
+        numero_chamado = 1
 
     titulo = input("Digite o título do chamado: ")
 
     with open("chamados.txt", "a") as arquivo:
-        arquivo.write(f"Chamado {numeros_chamados}: {titulo}\n")
+        arquivo.write(
+            f"Chamado {numero_chamado}: {titulo}\n"
+        )
+
+    print("Chamado aberto com sucesso!")
+    print("Número do chamado:", numero_chamado)
+
 
 def menu_inicial():
-    print("=====Bem vindo ao Help desk=====")
-
-    print("Escolha uma das opções abaixo:")
-    print("1 - login")
+    print("\n===== Bem vindo ao Help Desk =====")
+    print("1 - Login")
     print("2 - Cadastrar usuário")
-    print("3 - Abrir chamado")
-    print("4 - Consultar chamados")
-    print("5 - Sair")
-
-    numero_digitado = int(input("Digite o número da opção desejada: "))
-    if numero_digitado == 1:
-        abrir_chamado()
-
-    elif numero_digitado == 2:
-        print("Consultando chamados...")
-    elif numero_digitado == 3:
-        print("Saindo...")
-    else:
-        print("Opção inválida. Por favor, escolha uma opção válida.")
-
-def menu_estatico():
-    print("=====Bem vindo ao Help desk=====")
-
-    print("Escolha uma das opções abaixo:")
-    print("1 - Abrir chamado")
-    print("2 - Consultar chamados")
     print("3 - Sair")
 
+    numero_digitado = int(
+        input("Digite o número da opção desejada: ")
+    )
+
+    if numero_digitado == 1:
+        login()
+
+    elif numero_digitado == 2:
+        cadastrar_usuario()
+
+    elif numero_digitado == 3:
+        print("Saindo...")
+        return False
+
+    else:
+        print("Opção inválida.")
+
+    return True
+
+def menu_chamados():
+    while True:
+        print("\n===== Menu Help Desk =====")
+        print("1 - Abrir chamado")
+        print("2 - Consultar chamados")
+        print("3 - Sair")
+
+        numero_digitado = int(
+            input("Digite o número da opção desejada: ")
+        )
+
+        if numero_digitado == 1:
+            abrir_chamado()
+
+        elif numero_digitado == 2:
+            print("Consultando chamados...")
+
+        elif numero_digitado == 3:
+            print("Saindo...")
+            break
+
+        else:
+            print("Opção inválida.")
+
+
 def login():
-    nome_usuario = input("Digite seu nome de usuário: ")
     email = input("Digite seu email: ")
     senha = input("Digite sua senha: ")
-    print(f"Login realizado com sucesso para o usuário {nome_usuario}!")
+    try:
+        with open("usuarios.txt", "r") as arquivo:
+            usuarios = arquivo.readlines()
+
+        for usuario in usuarios:
+            nome_usuario, email_usuario, senha_usuario = usuario.strip().split(",")
+
+            if email == email_usuario and senha == senha_usuario:
+               print(f"\nLogin realizado com sucesso para o usuário {nome_usuario}!")
+               menu_chamados()
+               return
+        print("\nEmail ou senha incorretos. Tente novamente.")
+    except FileNotFoundError:
+        print("\nNenhum usuário cadastrado. Por favor, cadastre-se primeiro.")     
+    print(
+        f"\nLogin realizado com sucesso para o usuário "
+        f"{nome_usuario}!"
+    )
+
+    menu_chamados()
+
 
 def cadastrar_usuario():
     nome = input("Digite seu nome: ")
     email = input("Digite seu email: ")
     senha = input("Digite sua senha: ")
-    print(f"Usuário {nome} cadastrado com sucesso!")
+
+    print(
+        f"\nUsuário {nome} cadastrado com sucesso!")
+    
+    with open("usuarios.txt", "a") as arquivo:
+        arquivo.write(f"{nome},{email},{senha}\n")
+
 
 def main():
     while True:
-        menu_estatico()
-        op = int(input("Digite o número da opção desejada: "))
-        if op == 1:
-            print("Seja bem vindo!")
-            login()
-        elif op == 2:
-            print("Seja bem vindo de volta!")
-            menu_estatico()
-        else:
-            print("Opção inválida. Por favor, escolha uma opção válida.")    
+        continuar = menu_inicial()
+
+        if not continuar:
+            break
 
 
-       
 if __name__ == "__main__":
     main()
-
